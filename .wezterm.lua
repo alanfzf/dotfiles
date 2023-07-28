54f372a57737ae6e7fb9ac5bd66ce3c95393f093
alanfvn <alandavidg16@gmail.com>
2023-07-28 17:03:22 -0600

    Updated wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -48,14 +48,12 @@ config.leader = {
   timeout_milliseconds = 1000
 }
 config.keys = {
-  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-  { key = 'a', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' }},
   { key = "h", mods = "LEADER", action=wezterm.action({ActivatePaneDirection="Left"})},
   { key = "j", mods = "LEADER", action=wezterm.action({ActivatePaneDirection="Down"})},
   { key = "k", mods = "LEADER", action=wezterm.action({ActivatePaneDirection="Up"})},
   { key = "l", mods = "LEADER", action=wezterm.action({ActivatePaneDirection="Right"})},
-  { key = '-', mods = 'LEADER', action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' })},
-  { key = '=', mods = 'LEADER', action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' })},
+  { key = '=', mods = 'LEADER', action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' })},
+  { key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' })},
   { key = "]", mods="LEADER", action="TogglePaneZoomState" },
   { key = "p", mods="LEADER", action=wezterm.action({PasteFrom='Clipboard'})},
 }
