22dce6297cdd3a53a205fe3225a34fa59d3f1dae
alanfzf <alandavidg13@gmail.com>
2023-11-28 20:36:06 -0600

    Added more configs to linux

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@ sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
 
 sudo apt update
 sudo apt install build-essential -y
-
-# sudo apt install xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm chromium pulseaudio alsa-utils pavucontrol kitty
+sudo apt install pulseaudio pavucontrol kitty
