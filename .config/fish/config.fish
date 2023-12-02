3d672a7d8034e09438af37864af768291e802352
alanfzf <alandavidg13@gmail.com>
2023-12-02 03:12:12 -0600

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set fish_greeting ""
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias vi nvim
+alias gs "git status"
+alias cat batcat
+alias lg lazygit
+
+starship init fish | source
