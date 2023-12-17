f781c1961aacfb1a025393487172430440385a20
alanfzf <alandavidg16@gmail.com>
2023-12-16 20:27:41 -0600

    Started to work on bash install script

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
-sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
+#!/bin/bash
 
-sudo apt update
-sudo apt install build-essential -y
-sudo apt install pulseaudio pavucontrol kitty feh
+DOTFILES="$(dirname "$(readlink -f "$0")")"
+DOTS=(".xsessionrc" ".tmux.conf" ".ideavimrc")
+
+# ***** SETUP DOTFILES *****
+for item in "$DOTFILES/.config"/*; do
+  name=$(basename "$item")
+  target="$HOME/.config/$name"
+
+  rm -rf "$target"
+  ln -sf "$item" "$target"
+done
+
+
+for item in "${DOTS[@]}"; do
+  source="$DOTFILES/$item"
+  target="$HOME/$item"
+
+  rm -rf "$target"
+  ln -sf "$source" "$target"
+done
+
+#sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
+#sudo apt update
+#sudo apt install build-essential -y
+#sudo apt install pulseaudio pavucontrol kitty feh
