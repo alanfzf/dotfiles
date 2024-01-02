805c3d824007b3a0df1417cfe52d9db8dd00bb98
alanfvn <alandavidg16@gmail.com>
2024-01-01 23:59:31 -0600

    Updated colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,5 +2,7 @@ return {
   -- GOOD
   "https://github.com/Shatur/neovim-ayu",
   "https://github.com/joshdick/onedark.vim",
+  -- "https://github.com/shaunsingh/nord.nvim",
+  -- "https://github.com/gbprod/nord.nvim"
   -- "https://github.com/mjlbach/onedark.nvim",
 }
