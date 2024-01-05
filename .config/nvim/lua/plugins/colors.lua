f851252832449e1f219d59f0dfddef7f618dba8c
alanfvn <alandavidg16@gmail.com>
2024-01-04 21:12:10 -0600

    Updated some neovim plugins

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -4,5 +4,4 @@ return {
   "https://github.com/joshdick/onedark.vim",
   -- "https://github.com/shaunsingh/nord.nvim",
   -- "https://github.com/gbprod/nord.nvim"
-  -- "https://github.com/mjlbach/onedark.nvim",
 }
--- .config/nvim/lua/plugins/php.lua
+++ .config/nvim/lua/plugins/php.lua
@@ -1,4 +1,10 @@
 return {
-  "jwalton512/vim-blade",
-  ft="blade"
+  {
+    "jwalton512/vim-blade",
+    ft="blade"
+  },
+  {
+    "https://github.com/ccaglak/larago.nvim",
+    ft="blade"
+  }
 }
