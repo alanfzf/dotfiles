edb08c8b9ff4f37b43847c03a42e246914447103
alan <alandavidg13@gmail.com>
2024-01-17 22:41:43 -0600

    Updated fish and starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# env variables
+set -x VISUAL nvim
+set -x EDITOR nvim
+
+# other
 alias vi nvim
 alias gs "git status"
 alias cat batcat
