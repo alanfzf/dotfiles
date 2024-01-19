93f169d50661f896310f908a53b10b434ff757b9
alan <alandavidg13@gmail.com>
2024-01-18 23:46:23 -0600

    Updated neovim config

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,12 +2,7 @@ return {
   -- GOOD
   "https://github.com/Shatur/neovim-ayu",
   "https://github.com/joshdick/onedark.vim",
-  {
-    "https://github.com/craftzdog/solarized-osaka.nvim",
-    config = function ()
-      require("solarized-osaka").setup({ transparent = false, })
-    end
-  }
+  "https://github.com/Mofiqul/dracula.nvim",
   -- "https://github.com/shaunsingh/nord.nvim",
   -- "https://github.com/gbprod/nord.nvim"
 }
