a06fb461be80829c37c6d4f0f10c6cf3b9de5e11
alanfzf <alandavidg16@gmail.com>
2024-01-22 19:24:16 -0800

    Updated debian setup script

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+# Install qtile: https://www.youtube.com/live/Dk_UmUvKDrg?si=_n1OzLhtnTKvwRFD
+# Fix Wifi: https://www.reddit.com/r/debian/s/8V6hVfU8x1
+
 TEMPDIR=$(mktemp -d)
 NERDURL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
 
 # Basic window manager stuff 
-sudo apt install -y xorg xserver-xorg xclip python3 python3-pip python3-venv python3-v-sim python-dbus-dev libpangocairo-1.0-0 python3-xcffib python3-cairocffi libxkbcommon-dev libxkbcommon-x11-dev 
+sudo apt install -y \
+  xorg xserver-xorg xclip \
+  python3 python3-pip python3-venv python3-v-sim python-dbus-dev \
+  libpangocairo-1.0-0 python3-xcffib python3-cairocffi \
+  libxkbcommon-dev libxkbcommon-x11-dev
 
-# Basic opearting system stuff and greeter
-sudo apt install -y lightdm slick-greeter picom gvfs-backends pipewire pipewire-pulse 
+# Basic operating system stuff
+sudo apt instlal -y \
+  lightdm slick-greeter picom gvfs-backends \
+  pipewire pipewire-pulse
 
-# Install os management related apps
-sudo apt install -y fish thunar kitty feh scrot brightnessctl pulseaudio rofi volumeicon-alsa
+# Os management related apps
+sudo apt install -y \
+  fish thunar kitty feh scrot \
+  brightnessctl pulseaudio rofi volumeicon-alsa
 
-# Install tools
-sudo apt install -y wget curl fuse build-essential git gh fzf fd-find nodejs npm tmux bat ripgrep
+# Tools
+sudo apt install -y \
+  wget curl fuse build-essential git gh fzf \
+  fd-find nodejs npm tmux bat ripgrep
 
 cd $TEMPDIR
 
@@ -31,12 +44,6 @@ sudo update-grub
 # sudo sed -i 's/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
 # sudo apt remove ifupdown
 # systemctl restart network-manager
-
-# Disable audio power saving
-echo 0 > /sys/module/snd_hda_intel/parameters/power_save
-
-# Enable Bluetooth
-systemctl --user --now enable pipewire pipewire-pulse
 
 # Create desktop entry for qtile
 sudo cat > /usr/share/xsessions/qtile.desktop << EOF
@@ -58,3 +65,9 @@ EOF
 sudo cat > /etc/modprobe.d/alsa-base.conf <<EOF
 options snd-hda-intel power_save=0
 EOF
+
+# Disable audio power saving
+echo 0 > /sys/module/snd_hda_intel/parameters/power_save
+
+# Enable Bluetooth
+systemctl --user --now enable pipewire pipewire-pulse
