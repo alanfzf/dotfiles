3f52c49487adc9dee54bf196989cd5dc97cd6d2e
alan <alandavidg13@gmail.com>
2024-01-28 23:10:23 -0600

    Updated neovim plugins

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,1 +1,18 @@
-return {}
+return {
+  "https://github.com/junegunn/vim-easy-align",
+  {
+    "https://github.com/NvChad/nvim-colorizer.lua",
+    config = function ()
+      require("colorizer").setup {
+        filetypes = { "*" },
+        user_default_options = {
+          names = false,
+          mode = "background",
+          tailwind = true,
+          virtualtext = "■",
+          always_update = false
+        }
+      }
+    end
+  }
+}
