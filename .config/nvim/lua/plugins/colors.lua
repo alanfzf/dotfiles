1c217ee8a836b6fd4efad01820b7d7b5253ea743
alanfzf <alandavidg16@gmail.com>
2024-02-25 22:51:34 -0600

    Updated colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,5 +2,14 @@ return {
   -- GOOD
   "https://github.com/Shatur/neovim-ayu",
   "https://github.com/Mofiqul/dracula.nvim",
-  "https://github.com/gbprod/nord.nvim"
+  -- "https://github.com/gbprod/nord.nvim"
+  {
+    "https://github.com/svrana/neosolarized.nvim",
+    config = function ()
+      require('neosolarized').setup({})
+    end,
+    dependencies = {
+      "https://github.com/tjdevries/colorbuddy.nvim"
+    }
+  }
 }
