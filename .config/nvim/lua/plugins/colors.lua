dc0766e6bca945ddc1b3ad42039878cd6a3f1b86
alanfzf <alandavidg16@gmail.com>
2024-02-25 22:31:01 -0600

    Updated neovim config

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,5 @@ return {
   -- GOOD
   "https://github.com/Shatur/neovim-ayu",
   "https://github.com/Mofiqul/dracula.nvim",
-  -- "https://github.com/shaunsingh/nord.nvim",
-  -- "https://github.com/gbprod/nord.nvim"
+  "https://github.com/gbprod/nord.nvim"
 }
--- .config/nvim/lua/plugins/php.lua
+++ .config/nvim/lua/plugins/php.lua
@@ -5,6 +5,9 @@ return {
   },
   {
     "https://github.com/ccaglak/larago.nvim",
+    dependencies = {
+      "https://github.com/nvim-lua/plenary.nvim"
+    },
     ft="php"
   }
 }
