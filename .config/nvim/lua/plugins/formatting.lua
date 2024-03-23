38a7dfbd9323c8a9200c28077bf57369bf59d5db
alanfzf <alandavidg13@gmail.com>
2024-03-22 22:34:01 -0600

    Updated nvim config

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,24 +1,26 @@
 return {
   "stevearc/conform.nvim",
   config = function()
-    require('conform').setup({
+    require("conform").setup({
       formatters_by_ft = {
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
         lua = { "stylua" },
+        php = { "php_cs_fixer" },
+        blade = { "blade-formatter" },
       },
       format_on_save = {
         lsp_fallback = false,
         async = false,
-        timeout_ms = 500,
+        timeout_ms = 1500,
       },
       formatters = {
         prettier = {
-          require_cwd = true
-        }
-      }
+          require_cwd = true,
+        },
+      },
     })
-  end
+  end,
 }
