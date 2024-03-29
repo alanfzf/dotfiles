3d3c407d17f701bab98e40a3fb87e206cea0b232
alanfzf <alandavidg13@gmail.com>
2024-03-28 18:36:09 -0600

    :zap: (perf): Made fzf lua use bat instead of native previwer

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -3,49 +3,56 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("fzf-lua").setup({
-      file_icon_padding = ' ',
+      file_icon_padding = " ",
+
+      winopts = {
+        preview = {
+          default = "bat",
+          title = false,
+        },
+      },
+
       keymap = {
         builtin = {
           -- neovim `:tmap` mappings for the fzf win
-          ["<F1>"]  = "toggle-help",
-          ["<F2>"]  = "toggle-fullscreen",
+          ["<F1>"] = "toggle-help",
+          ["<F2>"] = "toggle-fullscreen",
           -- Only valid with the 'builtin' previewer
-          ["<F3>"]  = "toggle-preview-wrap",
-          ["<F4>"]  = "toggle-preview",
+          ["<F3>"] = "toggle-preview-wrap",
+          ["<F4>"] = "toggle-preview",
           -- Rotate preview clockwise/counter-clockwise
-          ["<F5>"]  = "toggle-preview-ccw",
-          ["<F6>"]  = "toggle-preview-cw",
+          ["<F5>"] = "toggle-preview-ccw",
+          ["<F6>"] = "toggle-preview-cw",
           ["<C-d>"] = "preview-page-down",
           ["<C-u>"] = "preview-page-up",
         },
         fzf = {
           -- fzf '--bind=' options
-          ["ctrl-z"]      = "abort",
-          ["ctrl-n"]      = "unix-line-discard",
-          ["ctrl-f"]      = "half-page-down",
-          ["ctrl-b"]      = "half-page-up",
-          ["ctrl-a"]      = "beginning-of-line",
-          ["ctrl-e"]      = "end-of-line",
+          ["ctrl-z"] = "abort",
+          ["ctrl-n"] = "unix-line-discard",
+          ["ctrl-f"] = "half-page-down",
+          ["ctrl-b"] = "half-page-up",
+          ["ctrl-a"] = "beginning-of-line",
+          ["ctrl-e"] = "end-of-line",
           -- Only valid with fzf previewers (bat/cat/git/etc)
-          ["f3"]          = "toggle-preview-wrap",
-          ["f4"]          = "toggle-preview",
-          ["ctrl-d"]  = "preview-page-down",
-          ["ctrl-u"]    = "preview-page-up",
+          ["f3"] = "toggle-preview-wrap",
+          ["f4"] = "toggle-preview",
+          ["ctrl-d"] = "preview-page-down",
+          ["ctrl-u"] = "preview-page-up",
         },
       },
       fzf_opts = {
-        ['--cycle'] = true,
-        ['--color'] = "bg+:-1",
+        ["--cycle"] = true,
+        ["--color"] = "bg+:-1",
       },
       previewers = {
         builtin = {
-          syntax          = true,         -- preview syntax highlight?
-          syntax_limit_l  = 0,            -- syntax limit (lines), 0=nolimit
-          syntax_limit_b  = 1024*1024,    -- syntax limit (bytes), 0=nolimit
-          limit_b         = 1024*1024*10, -- preview limit (bytes), 0=nolimit
+          syntax = true, -- preview syntax highlight?
+          syntax_limit_l = 0, -- syntax limit (lines), 0=nolimit
+          syntax_limit_b = 1024 * 1024, -- syntax limit (bytes), 0=nolimit
+          limit_b = 1024 * 1024 * 10, -- preview limit (bytes), 0=nolimit
         },
-      }
-
+      },
     })
-  end
+  end,
 }
