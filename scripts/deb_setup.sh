ef79368f067e6355cc086c2b055c857ef49bfc11
alanfzf <alandavidg13@gmail.com>
2024-05-01 17:55:02 -0600

    :wrench: (tweak): Deb setup created

--- scripts/deb_setup.sh
+++ scripts/deb_setup.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
+# VARS
 IS_WSL=$(grep -i Microsoft /proc/version)
 
-# Install non wsl packages
-if [[ ! $IS_WSL ]]; then
+# Do non wsl stuff
+if [[ $IS_WSL ]]; then
+  # Enable systemd
+  sudo tee /etc/wsl.conf > /dev/null <<EOF
+[boot]
+systemd=true
+EOF
 fi
 
+# Upgrade packages
+sudo apt update && sudo apt upgrade -y
 
-# enable systemd
-# sudo tee /etc/wsl.conf > /dev/null <<EOF
-# [boot]
-# systemd=true
-# EOF
+# Install base apps
+sudo apt install -y \
+  xclip zsh curl wget tmux bat \
+  build-essential git gh \
+  fzf fd-find ripgrep zoxide \
+  zip unzip
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Create ssh dir
+mkdir -p  ~/.ssh/
