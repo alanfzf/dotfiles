6f35c0c6099e826c5aee00bc6a467c02186aa752
alanfzf <alandavidg13@gmail.com>
2024-05-01 22:31:26 -0600

    :bug: (bugfix): Bad if condition when trying to get current path

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -13,7 +13,7 @@ local function get_root_dir()
   end
 
   local path = vim.fn.systemlist("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
+  if vim.v.shell_error == 0 then
     return path
   end
 
