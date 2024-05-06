4aeb3c5466511cb390e029997b16a8cccb2d3c90
alanfzf <alandavidg13@gmail.com>
2024-05-05 23:36:04 -0600

    :wrench: (tweak): Updated colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,14 +1,11 @@
 return {
   -- GOOD
-  { "https://github.com/romgrk/doom-one.vim" },
+  -- { "https://github.com/NTBBloodbath/doom-one.nvim", name = "doom-nvim" },
+  { "https://github.com/romgrk/doom-one.vim", name = "doom-vim" },
   { "https://github.com/catppuccin/nvim", name = "catppuccin" },
-  "https://github.com/mcauley-penney/ice-cave.nvim",
   -- solarizeds
-  { "https://github.com/EvenChang/vim", name = "xd" },
   -- other
-  { "https://github.com/andersevenrud/nordic.nvim", name = "nordo" },
+  { "https://github.com/andersevenrud/nordic.nvim", name = "good_nord" },
   { "https://github.com/embark-theme/vim", name = "embark" },
-  { "https://github.com/phha/zenburn.nvim" },
-  { "https://github.com/ellisonleao/gruvbox.nvim" },
   { "https://github.com/bluz71/vim-nightfly-colors" },
 }
