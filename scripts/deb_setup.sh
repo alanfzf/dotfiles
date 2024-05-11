e6bd2a88b8b227c702a38df8701ec9e2a31ec91a
alanfzf <alandavidg13@gmail.com>
2024-05-11 17:11:14 -0600

    :wastebasket: (removed): Remove nvm

--- scripts/deb_setup.sh
+++ scripts/deb_setup.sh
@@ -39,6 +39,3 @@ curl -L "https://github.com/starship/starship/releases/latest/download/starship-
 
 # 4.) Eza
 curl -L "https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz" | sudo tar xzf - -C /usr/local/bin/
-
-# 5.) nvm
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh" | bash
