fd4993ac1393a00aca77ff94a5271ce8c3baf006
alanfzf <alandavidg13@gmail.com>
2024-05-30 15:09:56 -0600

    :ambulance: (hotfix): Fixed breaking changes on qtile

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -1,5 +1,5 @@
 from libqtile.config import Key, Group
-from libqtile.command import lazy
+from libqtile.lazy import lazy  # Updated import for lazy
 from .keys import mod, keys
 
 groups = [Group(i) for i in [
@@ -10,7 +10,7 @@ for i, group in enumerate(groups):
     actual_key = str(i + 1)
     keys.extend([
         # Switch to workspace N
-        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        Key([mod], actual_key, lazy.group[group.name].toscreen(toggle=True)),
         # Send window to workspace N
-        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name, switch_group=True))
     ])
