9f2b2a6cb73f3e1589ce972d32e9ea22eb6c7dd2
alanfzf <alandavidg13@gmail.com>
2024-05-30 23:25:06 -0600

    :wrench: (tweak): Deleted some colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,9 +1,7 @@
 return {
   -- { "https://github.com/NTBBloodbath/doom-one.nvim", name = "doom-nvim" },
   { "https://github.com/romgrk/doom-one.vim", name = "doom-vim" },
-  -- other
   { "https://github.com/andersevenrud/nordic.nvim", name = "good_nord" },
   { "https://github.com/embark-theme/vim", name = "embark" },
   { "https://github.com/bluz71/vim-nightfly-colors" },
-  { "https://github.com/diegoulloao/neofusion.nvim" },
 }
