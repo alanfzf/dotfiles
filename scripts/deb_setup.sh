ff9d88663476b78d9000826d8bbebd9d05fc457c
alanfzf <alandavidg13@gmail.com>
2024-06-18 22:25:24 -0600

    :wrench: (tweak): Regain sudo for deb_setup

--- scripts/deb_setup.sh
+++ scripts/deb_setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
-fi
-
 # Upgrade packages
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 
 # Install base apps
-apt install -y \
+sudo apt install -y \
   zsh curl wget bat \
   build-essential git gh \
   fzf fd-find ripgrep zoxide \
@@ -17,15 +12,15 @@ apt install -y \
 
 # === Install non apt packages ===
 # 1.) Neovim
-curl -L "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz" | tar xzf - -C /opt/
-ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+curl -L "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz" | sudo tar xzf - -C /opt/
+sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
 
 # 2.) Lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | tar xzf - -C /usr/local/bin/
+curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | sudo tar xzf - -C /usr/local/bin/
 
 # 3.) Starship
 curl -fsSL https://starship.rs/install.sh | sh -s -- --yes
 
 # 4.) Eza
-curl -L "https://github.com/eza-community/eza/releases/download/v0.18.17/eza_x86_64-unknown-linux-gnu.tar.gz" | tar xzf - -C /usr/local/bin/
+sudo curl -L "https://github.com/eza-community/eza/releases/download/v0.18.17/eza_x86_64-unknown-linux-gnu.tar.gz" | tar xzf - -C /usr/local/bin/
