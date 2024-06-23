00545d76afd06c7cddabe9366dacadeb22082faf
alanfzf <alandavidg13@gmail.com>
2024-06-23 00:32:21 -0600

    style: updated misc plugins for nvim

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,29 +1,29 @@
 return {
+  { "junegunn/vim-easy-align" },
+  {
+    "razak17/tailwind-fold.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
+  },
   {
     "cbochs/grapple.nvim",
     dependencies = {
       { "nvim-tree/nvim-web-devicons", lazy = true },
     },
   },
-  { "https://github.com/junegunn/vim-easy-align" },
   {
-    "https://github.com/brenoprata10/nvim-highlight-colors",
+    "brenoprata10/nvim-highlight-colors",
     config = function()
       require("nvim-highlight-colors").setup({
         render = "background",
         virtual_symbol = "■",
-        enable_named_colors = true,
+        enable_named_colors = false,
         enable_tailwind = false,
       })
     end,
   },
   {
-    "https://github.com/razak17/tailwind-fold.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
-  },
-  {
-    "https://github.com/LunarVim/bigfile.nvim",
+    "LunarVim/bigfile.nvim",
     config = function()
       require("bigfile").setup({})
     end,
