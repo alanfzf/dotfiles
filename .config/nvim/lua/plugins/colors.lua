919cdb958eb0a76db4ba804bde9b8219568ee1ca
alanfzf <alandavidg13@gmail.com>
2024-06-24 01:31:18 -0600

    chore: Updated colorschemes

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,8 @@
 return {
-  -- { "https://github.com/NTBBloodbath/doom-one.nvim", name = "doom-nvim" },
-  { "https://github.com/romgrk/doom-one.vim", name = "doom-vim" },
-  { "https://github.com/andersevenrud/nordic.nvim", name = "good_nord" },
-  { "https://github.com/embark-theme/vim", name = "embark" },
+  { "https://github.com/Shatur/neovim-ayu" },
+  { "https://github.com/andersevenrud/nordic.nvim" },
   { "https://github.com/bluz71/vim-nightfly-colors" },
   { "https://github.com/lunacookies/vim-colors-xcode" },
+  { "https://github.com/mellow-theme/mellow.nvim" },
+  { "https://github.com/nyoom-engineering/oxocarbon.nvim" },
 }
