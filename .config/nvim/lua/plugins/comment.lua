f3199902f53d91e983bf4275617190ad82408948
alanfzf <alandavidg13@gmail.com>
2024-07-16 22:04:22 -0600

    feat(nvim)!: remove ai plugin and change comment plugin

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,11 +1,19 @@
 return {
-  {
-    "folke/ts-comments.nvim",
-    opts = {
-      lang = {
-        blade = "{{-- %s --}}",
-      },
-    },
-    event = "VeryLazy",
-  },
+  "https://github.com/JoosepAlviste/nvim-ts-context-commentstring",
+  config = function()
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+      custom_calculation = function(node, language_tree)
+        if vim.bo.filetype == "blade" and language_tree._lang ~= "javascript" then
+          return "{{-- %s --}}"
+        end
+      end,
+    })
+
+    local get_option = vim.filetype.get_option
+    vim.filetype.get_option = function(filetype, option)
+      return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+        or get_option(filetype, option)
+    end
+  end,
 }
