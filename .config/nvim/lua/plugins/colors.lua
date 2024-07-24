25747f02d9432522e41b17eac84b720f0356006e
alanfzf <alandavidg13@gmail.com>
2024-07-23 22:25:12 -0600

    feat(nvim): remove colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,5 @@
 return {
   { "https://github.com/Shatur/neovim-ayu" },
-  { "https://github.com/andersevenrud/nordic.nvim" },
   { "https://github.com/bluz71/vim-nightfly-colors" },
   { "https://github.com/lunacookies/vim-colors-xcode" },
-  { "https://github.com/mellow-theme/mellow.nvim" },
-  { "https://github.com/nyoom-engineering/oxocarbon.nvim" },
 }
