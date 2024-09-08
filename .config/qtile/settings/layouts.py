01c87cce5ade641c66239079855f240b933c85dc
alanfzf <alandavidg13@gmail.com>
2024-09-08 12:37:57 -0600

    feat(qtile): update qtile layouts

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -1,16 +1,19 @@
 from libqtile import layout
-from libqtile.config import Match
 from .theme import colors
 
 # Layouts and layout rules
 normal_style = {
     'border_focus': colors['focus'][0],
+    'border_normal': colors['inactive'][0],
     'border_width': 2,
+    'margin': 5,
 }
 
 float_style = {
     'border_width': 2,
-    'border_focus': colors['color3'][0]
+    'border_focus': colors['color3'][0],
+    'border_normal': colors['inactive'][0],
+    'margin': 5,
 }
 
 layouts = [
@@ -18,18 +21,11 @@ layouts = [
     layout.MonadWide(**normal_style),
     layout.Matrix(**normal_style, columns=2),
     layout.RatioTile(**normal_style),
-    layout.Max(),
 ]
 
 floating_layout = layout.Floating(
     **float_style,
     float_rules = [
         *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),
-        Match(wm_class="makebranch"),
-        Match(wm_class="maketag"),
-        Match(wm_class="ssh-askpass"),
-        Match(title="branchdialog"),
-        Match(title="pinentry"),
     ]
 )
