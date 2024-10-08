3f65d1a0fc1f24140cf04942806a8e3f50ef7a8c
alanfzf <alandavidg13@gmail.com>
2024-10-08 01:14:20 -0600

    feat(debian): add basic script for debian

---
+++ scripts/debian_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+
+sudo apt-get install -y tmux git zsh build-essential curl wget gh zoxide zip unzip fzf fd-find ripgrep bat exa
+
+# symlink magic
+sudo ln -sf /usr/bin/batcat /usr/local/bin/bat
+sudo ln -sf /bin/fdfind /usr/local/bin/fd
+sudo ln -sf /usr/bin/exa /usr/local/bin/eza
+
+# install neovim
+curl -L "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz" | sudo tar xzf - -C /opt/
+sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | sudo tar xzf - -C /usr/local/bin/
+
+# install starship
+curl -fsSL https://starship.rs/install.sh | sh -s -- --yes
