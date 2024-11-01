4b7fc8c2b792b0b05f62fb48fb23de86311f986a
alanfzf <alandavidg13@gmail.com>
2024-10-31 20:36:01 -0600

    feat(nvim)!: delete unused colors

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,4 @@ return {
   { "https://github.com/ellisonleao/gruvbox.nvim" },
   { "https://github.com/NTBBloodbath/doom-one.nvim" },
   { "https://github.com/maxmx03/solarized.nvim" },
-  { "https://github.com/phha/zenburn.nvim" },
-  { "https://github.com/kxzk/skull-vim" },
 }
