2b2334455ded1be7f08269454ce5fa2555802e54
alanfzf <alandavidg13@gmail.com>
2024-11-01 21:16:20 -0600

    feat(home-manager): add direnv support

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -84,6 +84,10 @@
     enable = true;
   };
 
+  programs.direnv = {
+    enable = true;
+  };
+
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
@@ -92,6 +96,21 @@
       "alan" = {
         hostname = "pruebas.alanfzf.site";
         user = "ubuntu";
+      };
+      "ftp" = {
+        hostname = "ftp.finanssoreal.com";
+        user = "ubuntu";
+        identityFile = "~/.ssh/ed25519_finanssoreal";
+      };
+      "testing" = {
+        hostname = "testing.finanssoreal.com";
+        user = "debian";
+        identityFile = "~/.ssh/ed25519_finanssoreal";
+      };
+      "prod" = {
+        hostname = "control.finanssoreal.com";
+        user = "debian";
+        identityFile = "~/.ssh/ed25519_finanssoreal";
       };
     };
   };
