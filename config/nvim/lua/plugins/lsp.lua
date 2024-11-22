f527f58e46173e48a6f317e1da01a5a31d786d14
alanfzf <alandavidg13@gmail.com>
2024-11-22 09:06:34 -0600

    feat(nvim): update nvim settings

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -76,6 +76,19 @@ local setup_lsp_servers = function()
   }
 
   vim.diagnostic.config(config)
+
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- Disable underline, it's very annoying
+    underline = false,
+    virtual_text = true,
+    -- Enable virtual text, override spacing to 4
+    -- virtual_text = {spacing = 4},
+    -- Use a function to dynamically turn signs off
+    -- and on, using buffer local variables
+    signs = true,
+    update_in_insert = false,
+  })
+
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
   })
