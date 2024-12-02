54b8fed4a6608ec14fc570435718c41dcc9fe3b5
alanfzf <alandavidg13@gmail.com>
2024-12-01 23:14:59 -0600

    feat(nvim): remove colorschemes

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,5 @@ return {
   { "https://github.com/joshdick/onedark.vim" },
   { "https://github.com/ellisonleao/gruvbox.nvim" },
   { "https://github.com/maxmx03/solarized.nvim" },
-  { "https://github.com/lunacookies/vim-colors-xcode" },
   { "https://github.com/polirritmico/monokai-nightasty.nvim" },
 }
