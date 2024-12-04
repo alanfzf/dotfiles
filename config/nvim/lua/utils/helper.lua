1d06ef5a0413beb4bbc88420633d41d37aa118fc
alanfzf <alandavidg13@gmail.com>
2024-12-04 16:49:53 -0600

    feat(nvim): tweak helper file

--- config/nvim/lua/utils/helper.lua
+++ config/nvim/lua/utils/helper.lua
@@ -2,14 +2,6 @@ local M = {}
 
 M.get_root_dir = function()
   local lsp_loc = vim.lsp.buf.list_workspace_folders()[1]
-  local status, oil = pcall(require, "oil")
-
-  if status then
-    local root = oil.get_current_dir()
-    if root then
-      return root
-    end
-  end
 
   if lsp_loc then
     return lsp_loc
