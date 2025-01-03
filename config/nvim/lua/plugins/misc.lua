fa25b2a2ab490154304fc3b9fef3a4022df34fd2
alanfzf <alandavidg13@gmail.com>
2025-01-03 08:51:25 -0600

    feat(nvim): make arrow.nvim default action to open new tabs

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -16,6 +16,11 @@ return {
       show_icons = true,
       leader_key = ";",
       buffer_leader_key = "m",
+      custom_actions = {
+        open = function(target_file_name)
+          vim.cmd("tabnew " .. target_file_name)
+        end,
+      },
     },
   },
   {
