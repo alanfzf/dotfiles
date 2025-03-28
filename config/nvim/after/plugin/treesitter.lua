e1b8104d893e27025f82bcb75f35a93d7b94b753
alanfzf <alandavidg13@gmail.com>
2025-03-28 09:28:39 -0600

    feat(nvim): delete blade parser as its now part of nvim-treesitter

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -3,15 +3,6 @@ local install_opts = require("nvim-treesitter.install")
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 install_opts.prefer_git = false
 
-parser_config.blade = {
-  filetype = "blade",
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-}
-
 -- basic treesitter setup
 ts.setup({
   auto_install = true,
