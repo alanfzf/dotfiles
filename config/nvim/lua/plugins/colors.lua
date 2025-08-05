452a65d34ad91bd7915bfd29ce97e231c83356ac
alanfzf <alandavidg13@gmail.com>
2025-08-04 22:55:23 -0600

    feat(nvim): remove unused colors

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,7 +1,5 @@
 return {
   { "lifepillar/vim-solarized8", branch = "neovim" },
   { "joshdick/onedark.vim" },
-  { "idr4n/github-monochrome.nvim" },
-  { "lunacookies/vim-colors-xcode" },
   { "folke/tokyonight.nvim" },
 }
