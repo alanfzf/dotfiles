ca1f18d9fdb1b0a506963b1e5fdd87b6451150dd
alanfzf <alandavidg13@gmail.com>
2025-08-14 21:41:49 -0600

    feat(git): adjust git config

--- config/nvim/plugin/usercmds.lua
+++ config/nvim/plugin/usercmds.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_user_command("PackUpdate", function()
+  local inactive = {}
+
+  for _, plugin in ipairs(vim.pack.get()) do
+    if not plugin.active then
+      table.insert(inactive, plugin.spec.name)
+    end
+  end
+  vim.pack.del(inactive)
+  vim.pack.update()
+end, { nargs = 0 })
+
 -- remove annoying windows line endings
 vim.api.nvim_create_user_command("FixM", function()
   vim.api.nvim_exec2("edit ++ff=dos %", {})
