cefb138344db9842ca1441785585e00f86d965fe
alanfzf <alandavidg13@gmail.com>
2025-09-26 10:42:31 -0600

    feat(nixos): adjust stuff

--- config/nixos/home-manager/home.nix
+++ config/nixos/home-manager/home.nix
@@ -208,4 +208,17 @@ in
       Include ~/.ssh/devpod
     '';
   };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
 }
