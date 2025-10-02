3f5ef387e752f0f1e704b4c18cd29913ae68cf37
alanfzf <alandavidg13@gmail.com>
2025-10-02 15:33:23 -0600

    feat(misc): adjust configs

--- config/nixos/nix/packages.nix
+++ config/nixos/nix/packages.nix
@@ -28,14 +28,7 @@
   programs.firefox = {
     enable = true;
     preferences = {
-      "browser.cache.disk.enable" = false;
-      "browser.cache.memory.enable" = false;
-      "browser.sessionhistory.max_entries" = 5;
-      "browser.sessionhistory.max_total_viewers" = 0;
-      "browser.sessionstore.interval" = 600000;
-      "extensions.pocket.enabled" = false;
       "general.autoScroll" = true;
-      "network.prefetch-next" = false;
     };
   };
 
